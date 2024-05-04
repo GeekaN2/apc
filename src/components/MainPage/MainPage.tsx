80d30be09ac0f4ee1ccb2c008fc432b48686a451
geekan <geekan@bk.ru>
2024-05-04 20:47:09 +0300

    feat: change index page

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,17 +1,19 @@
 import { Button, Text } from '@gravity-ui/uikit'
 import styles from './MainPage.module.css'
 import { Footer } from './Footer/Footer'
+import { useNavigate } from 'react-router-dom'
 
 export const MainPage: React.FC = () => {
+  const navigate = useNavigate();
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <img src='/assets/favicon.png' className={styles.logo}></img>
+        <img src='/public/favicon.png' className={styles.logo}></img>
         <Text variant='display-4' color='complementary'>Albion Profit Calculator</Text>
       </div>
       <div className={styles.pages}>
-        <Button className={styles.pageButton} view='outlined' size='l'>🌳 Profit tree</Button>
+        <Button className={styles.pageButton} view='outlined' size='l' onClick={() => navigate('/tree')}>🌳 Profit tree</Button>
         <Button className={styles.pageButton} view='outlined' size='l'>🐗 Transportations</Button>
         <Button className={styles.pageButton} view='outlined' size='l'>♻️ Refining</Button>
         <Button className={styles.pageButton} view='outlined' size='l'>🍲 Food & potions</Button>
